21603098fb0dc0ea065143040514580876f27dc8
Luis Alberto <nelixhost@gmail.com>
2015-04-23 16:40:08 -0500

    css extra
    I-ve add some extra css rules :)

--- js/main.js
+++ js/main.js
@@ -3,38 +3,58 @@
 
 //Setting Global
 
+jQuery.support.cors = true;
 window.ws = "http://172.18.149.21/Servicios/REST.svc/";
 
 
-$(document).ready(function(){
+//$(document).ready(function(){
+$(window).load(function() {
 
-  $(".date").text("Abril, 2015");
+  //$(".date").text("Abril, 2015");
 
-  function formatAMPM(){
+  //if($('#view-container').length){
+    //$('#view-container').load('views/login.html');
+  //}
 
-    var d = new Date(),
-
-      minutes = d.getMinutes().toString().length == 1 ? '0'+ d.getMinutes() : d.getMinutes(),
-      hours = d.getHours().toString().length == 1 ? '0'+ d.getHours() : d.getHours(),
-      ampm = d.getHours() >= 12 ? 'pm' : 'am',
-      months = ['Enero','Febrero','Marzo','April','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-      days = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
-
-    return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
-  }
-
-  if($('#view-container').length){
-    //$('#view-container').load('views/main.html');
-  }
-
+  /*
   $("#page1").click(function(){
     $('#view-container').load('views/main.html');
     alert("Thanks for visiting!");
   });
+  */
 
 });
 
 //login
+
+function login(user, password){
+
+  var url = ws+"rp_seguridadLogin";
+
+  var oData = { User: user, Password: password };
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    crossDomain: true,
+    data: JSON.stringify(oData),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: OnSuccessCall_WS,
+    error: OnErrorCall_WS
+  });
+
+  function OnSuccessCall_WS(response) {
+     alert(response);
+     $("#logdIn").fadeOut().hide();//.css("display","none");
+     $("#main").fadeIn().show();
+  }
+  function OnErrorCall_WS(response) {
+    alert("Error: " + response.status + " " + response.statusText);
+  }
+
+}
+
 
 $(document).ready(function(){
 
@@ -43,37 +63,20 @@ $(document).ready(function(){
 
     $("#login").click(function(){
 
+      $("#login").prop( "disabled", true );
+
       var user = $('#user').val();
       var password = $('#password').val();
 
       if($("#user").val() == '' || $("#password").val() == ''){
-
         alert("Los campos son obligatorios");
-
       }else{
 
-        var url = ws+"rp_seguridadLogin";
+        login(user, password);
+        document.getElementById("user").reset();
+        document.getElementById("password").reset();
 
-        var oData = {
-          User: user,
-          Password: password
-        };
 
-        $.ajax({
-          type: "POST",
-          url: url,
-          crossDomain: true,
-          data: JSON.stringify(oData),
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-          success: function(data){
-            alert(data);
-          },
-          error: function(data){
-            console.info("Error");
-          }
-
-        });
       }
 
     });
